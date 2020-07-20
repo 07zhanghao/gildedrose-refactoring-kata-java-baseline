3a204e5a8dcab4217011bb7f5a897c60ad6c924e
zhanghao <zhanghao07@picclife.cn>
2020-07-20 18:25:18 +0800

    CombinationApprovals

--- src/test/java/gildedrose/GildedRoseTest.java
+++ src/test/java/gildedrose/GildedRoseTest.java
@@ -1,17 +1,26 @@
 package gildedrose;
 
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
 
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[]{new Item("foo", 0, 0)};
+    public void updateQuality() {
+        CombinationApprovals.verifyAllCombinations(
+                this::doUpdateQuality,
+                new String[]{"foo","Aged Brie","Backstage passes to a TAFKAL80ETC concert","Sulfuras, Hand of Ragnaros",},
+                new Integer[]{-1,0,1,4,5,6,9,10,11},
+                new Integer[]{0,1,46,47,48,49,50}
+        );
+    }
+
+    private String doUpdateQuality(String name, int sellIn, int quality) {
+        Item[] items = new Item[]{new Item(name, sellIn, quality)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+//        assertEquals("foo", app.items[0].name);
+        return app.items[0].toString();
     }
 
 }
